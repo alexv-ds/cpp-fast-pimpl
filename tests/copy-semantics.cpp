283be3a10c8bed0e0b899b62bd03138752a3034d
alexv-ds <sasichkamega@gmail.com>
2024-01-20 20:10:03 +0300

    added test

--- tests/copy-semantics.cpp
+++ tests/copy-semantics.cpp
@@ -21,4 +21,14 @@ TEST_CASE("'fpimpl::operator=(const fpimpl& pimpl)' copies correctly") {
   REQUIRE((*pimpl_2) == "TEST_12");
 }
 
+TEST_CASE("'fpimpl::operator=(const fpimpl& pimpl)' correctly copies with self copy") {
+  fpimpl<std::string, sizeof(std::string), alignof(std::string)> pimpl("SELF COPY");
+  void* ptr1 = &*pimpl;
+  pimpl = pimpl;
+  void* ptr2 = &*pimpl;
+
+  REQUIRE(ptr1 == ptr2);
+  REQUIRE((*pimpl) == "SELF COPY");
+}
+
 }
